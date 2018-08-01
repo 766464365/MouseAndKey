eeec1b97e0df6b384f92e17e7d04fa6f20d57a74
john <766464365@qq.com>
2018-08-01 16:40:02 +0800

    完成到T14，准备做15

--- src/fingerSite/test11/Solution.java
+++ src/fingerSite/test11/Solution.java
@@ -8,7 +8,21 @@ package fingerSite.test11;
 //输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
 public class Solution {
     public int NumberOf1(int n) {
-
+        String str=String.valueOf(Integer.toBinaryString(n));
+        char[]  chars=str.toCharArray();
+        int num=0;
+        for (char x:chars
+                ) {
+            if(x=='1'){
+                num++;
+            }
+        }
+        return num;
+    }
+    public static void main(String[] args){
+        System.out.println(Integer.toBinaryString(13));
+        Solution solution=new Solution();
+        System.out.println(solution.NumberOf1(13));
     }
 }
 
---
+++ src/fingerSite/test15/Solution.java
@@ -0,0 +1,24 @@
+/*
+ *While writing this code, only God and I know what it is.
+ *And now only God knows it
+ *____________________________________by xuwei
+ */
+package fingerSite.test15;
+//输入一个链表，反转链表后，输出新链表的表头。
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode ReverseList(ListNode head) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
