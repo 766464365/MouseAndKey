eaff326b8990a4bcfe9ea0164a0cc903408389c9
xuwei <766464365@qq.com>
2018-08-04 21:48:59 +0800

    a update in dataStructure/sortTest

---
+++ src/dataStructor/sort/Solution.java
@@ -0,0 +1,71 @@
+/*
+ *While writing this code, only God and I know what it is.
+ *And now only God knows it
+ *___________________________by xuwei
+ */
+
+package dataStructor.sort;
+
+import java.util.Scanner;
+
+public class Solution {
+    /** 请完成下面这个函数，实现题目要求的功能 **/
+    /** 当然，你也可以不按照这个模板来作答，完全按照自己的想法来 ^-^  **/
+    public static int[] selectionSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int k = i;
+            // 找出最小值的小标
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[k]) {
+                    k = j;
+                }
+            }
+            // 将最小值放到排序序列末尾
+            if (k > i) {
+                int tmp = a[i];
+                a[i] = a[k];
+                a[k] = tmp;
+            }
+        }
+        return a;
+    }
+    static int maxWorkinghourGap(int[] workinghours) {
+        int[] sortArray =selectionSort(workinghours);
+        int length=sortArray.length;
+        int distance=0;
+        if(length>1){
+            for(int i=0;i<length-1;i++){
+                int tempDistance=sortArray[i+1]-sortArray[i];
+                if(tempDistance>distance){
+                    distance=tempDistance;
+                }
+
+            }
+            return distance;
+        }
+        else
+            return 0;
+
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String inputData;
+        try {
+            inputData = in.nextLine();
+        } catch (Exception e) {
+            return;
+        }
+
+        String[] strs = inputData.split(",");
+        int[] workinghours = new int[strs.length];
+        for (int i=0;i<strs.length;i++) {
+            workinghours[i] = Integer.parseInt(strs[i]);
+        }
+
+        int res = maxWorkinghourGap(workinghours);
+        System.out.println(String.valueOf(res));
+
+    }
+}
--- src/fingerSite/test11/Solution.java
+++ src/fingerSite/test11/Solution.java
@@ -7,8 +7,8 @@
 package fingerSite.test11;
 //输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
 public class Solution {
-    public int NumberOf1(int n) {
-
-    }
+//    public int NumberOf1(int n) {
+//
+//    }
 }
 
