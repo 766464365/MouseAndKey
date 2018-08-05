4daac769382f6bfec9dbbe92ff16cf9a068b3fe2
xuwei <766464365@qq.com>
2018-08-05 19:02:32 +0800

    begin test17

---
+++ src/fingerSite/test17/Solution.java
@@ -0,0 +1,24 @@
+/*
+ *While writing this code, only God and I know what it is.
+ *And now only God knows it
+ *___________________________by xuwei
+ */
+
+package fingerSite.test17;
+//输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+public class Solution {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+
+    }
+}
