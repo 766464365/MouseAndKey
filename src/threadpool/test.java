9c0691506a4fab032d37d7fce68f88a774e1de9d
john <766464365@qq.com>
2018-08-14 18:35:49 +0800

    add test18.19

---
+++ src/threadpool/test.java
@@ -0,0 +1,14 @@
+/*
+ *While writing this code, only God and I know what it is.
+ *And now only God knows it
+ *____________________________________by xuwei
+ */
+package threadpool;
+
+public class test {
+    public static void main(String args[]){
+        int CAPACITY =Integer.SIZE-3;
+        int a=-1<<29;
+        System.out.println(Integer.toBinaryString(a));
+    }
+}
